2420ae377b5c8f815c9a57d35c5691da505993b5
Vishnu <vishnunandana8@gmail.com>
2022-05-23 21:46:53 +0530

    Rollback commit

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.chatapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "Settings CLicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topmenu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/example/chatapplication/PhoneNumberActivity.java
+++ app/src/main/java/com/example/chatapplication/PhoneNumberActivity.java
@@ -1,22 +1,31 @@
 package com.example.chatapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.chatapplication.databinding.ActivityPhoneNumberBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PhoneNumberActivity extends AppCompatActivity {
     ActivityPhoneNumberBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= ActivityPhoneNumberBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        auth=FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser()!=null) {
+            Intent intent = new Intent(PhoneNumberActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
         binding.phoneNumber.requestFocus();
         binding.continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
