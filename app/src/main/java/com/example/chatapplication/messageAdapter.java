b4c71ca35c651c91128f1be3f4965229aa96cf79
Vishnu <vishnunandana8@gmail.com>
2022-05-29 11:00:51 +0530

    Basic Chat Activity added

---
+++ app/src/main/java/com/example/chatapplication/messageAdapter.java
@@ -0,0 +1,87 @@
+package com.example.chatapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatapplication.databinding.ItemRecieveBinding;
+import com.example.chatapplication.databinding.ItemSentBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+public class messageAdapter extends RecyclerView.Adapter {
+    Context context;
+    ArrayList<message> messages;
+
+    final int ITEM_SENT = 1;
+    final int ITEM_RECEIVE = 2;
+
+    public messageAdapter(Context context, ArrayList<message> messages) {
+        this.context=context;
+        this.messages=messages;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(viewType == ITEM_SENT) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_sent, parent ,false);
+                    return new SentViewHolder(view);
+        }else{
+            View view= LayoutInflater.from(context).inflate(R.layout.item_recieve, parent, false);
+            return new ReceiverViewHolder(view);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        message message = messages.get(position);
+        if(FirebaseAuth.getInstance().getUid().equals(message.getSenderId())) {
+            return ITEM_SENT;
+        }else {
+            return ITEM_RECEIVE;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        message message = messages.get(position);
+
+        if(holder.getClass() == SentViewHolder.class) {
+            SentViewHolder viewHolder = (SentViewHolder)holder;
+            viewHolder.binding.message.setText(message.getMessage());
+        }else {
+            ReceiverViewHolder viewHolder = (ReceiverViewHolder) holder;
+            viewHolder.binding.message.setText(message.getMessage());
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    public class SentViewHolder extends RecyclerView.ViewHolder {
+
+        ItemSentBinding binding;
+        public SentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemSentBinding.bind(itemView);
+
+        }
+    }
+    public class ReceiverViewHolder extends RecyclerView.ViewHolder {
+
+        ItemRecieveBinding binding;
+        public ReceiverViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemRecieveBinding.bind(itemView);
+        }
+    }
+}
